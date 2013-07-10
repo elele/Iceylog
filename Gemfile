54b36222ecfcebad0ee7096fc8cd3daa1b814637
段磊 <elele.cool@gmail.com>
2013-07-10 11:24:31 +0800

    update

--- Gemfile
+++ Gemfile
@@ -1,11 +1,11 @@
 source 'http://ruby.taobao.org'
 
 gem 'execjs'
-gem 'therubyracer'
+# gem 'therubyracer'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
-gem 'rails', '3.2.3'
+gem 'rails', '3.2.13'
 gem "rails_autolink", ">= 1.0.4"
 
 # Mongoid 辅助插件
@@ -53,7 +53,7 @@ group :assets do
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
   # gem 'therubyracer', :platform => :ruby
 
-  gem 'uglifier', '>= 1.0.3'
+  # gem 'uglifier', '>= 1.0.3'
 end
 
 gem 'jquery-rails'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 # coding: utf-8
 
 # 默认配置项
@@ -5,16 +7,16 @@
 
 
 # site title ,slogan and author HTML
-SiteConfig.save_default("site_title","Iceylog")
-SiteConfig.save_default("site_slogan","Focus on Android,iPhone,Web,Business,Architecture,Agile,Technic and beyond…")
-SiteConfig.save_default("site_author","Icey")
-SiteConfig.save_default("site_author_username","iceylog")
-SiteConfig.save_default("site_author_password","password")
+SiteConfig.save_default("site_title","elele")
+SiteConfig.save_default("site_slogan","技术型纯diao")
+SiteConfig.save_default("site_author","Elele")
+SiteConfig.save_default("site_author_username","elele")
+SiteConfig.save_default("site_author_password","admin519728")
 
 # Footer HTML
 SiteConfig.save_default("about_me_html",<<-eos
 <div class="about">
-  <p>IceskYsl, 简称Ice, 80后, 典型巨蟹男, 移动互联网创业者; Google产品重度依赖者, Mac, Android, iPhone, BB 非典型用户;关注创新,技术,产品和一切新奇的玩意儿; <br />求学武汉, 毕业南下深圳, 尔后北漂在京, 至今数年有余; 追寻内心的想法, 不随波逐流, 爱折腾, 爱旅行, 孩子气, 享受工作, 安静的做喜欢的事情...</p>
+  <p>移动互联网底层小diao </p>
 </div>
 eos
 )
@@ -22,7 +24,7 @@ eos
 # menu HTML
 SiteConfig.save_default("menu_html",<<-eos
   	<ul>
- 			<li><a href="/">Index</a></li>
+ 			<li><a href="/">首页</a></li>
 			<li><a href="/page/book">Book</a></li>
 			<li><a href="/page/movie">Movie</a></li>
 			<li><a href="/page/music">Music</a></li>
@@ -36,12 +38,11 @@ eos
 
 # Footer HTML
 SiteConfig.save_default("footer_html",<<-eos
-  <p>Copyright &copy;2007-2012 - Lovingly authored by IceskYsl - All my work is <a href="http://www.opensource.org/licenses/MIT">MIT licensed, Open and Free</a>. </p>
+  <p>Copyright &copy;2008-2013 - Lovingly authored by Elele - All my work is <a href="http://www.opensource.org/licenses/MIT">MIT licensed, Open and Free</a>. </p>
   <p>
-    Powered by <a href="http://www.iceskysl.com/" target="_blank" title="Iceylog is a blog  in Ruby">Iceylog</a> - Theme by <a href="http://www.iceskysl.com/" target="_blank" title="iceskysl's blog.">IceskYsl@1.s.t</a> 
+    Powered by <a href="http://www.elele.me/" target="_blank" title="Elele is a blog  in Ruby">Elele</a> - Theme by <a href="http://www.elele.me/" target="_blank" title="Elele's blog.">@elele</a> 
     - Build by <a href="http://daringfireball.net/projects/markdown/" target="_blank" title="Markdown is a text-to-HTML conversion tool for web writers">Markdown</a>,
-    <a href="https://github.com/" target="_blank" title="GitHub">GitHub</a>,  
-    <a href="http://disqus.com/" target="_blank" title="Disqus is a global comment system that improves discussion on websites and connects conversations across the web.">Disqus</a>
+    <a href="https://github.com/elele" target="_blank" title="GitHub">GitHub</a>,  
   </p>	
 eos
 )
@@ -63,4 +64,4 @@ Page.create(:slug=>"toolkit", :title => "Toolkit")
 Post.create(:category_id => c.id, :title => "blog post title", :body => "This is a new post body,support **Markdown** format..",:tag_list => "demo,tag")
 
 #Site
-Site.create(:name => "Iceskysl Blog",:url => "http://www.iceskysl.com")
+Site.create(:name => "Elele Blog",:url => "http://www.elele.me")
